7c953254ed8395acecbf3aff6ee75667f0697b3c
Bjorn Ohlsson <bjorn.ohlsson@glimworks.com>
2017-12-04 13:34:20 +0100

    Update

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const simpleGit = require("simple-git/promise");
 const path = require('path');
 const basePath = '/var/www';
 const repoInfo = require('./repos-and-branches.json');
+const pm = require('promisemaker');
+const exec = pm(require('child_process').exec);
 
 // Checkout a branch and make a pull for that branch
-async function pull(repoPath, branch){
+async function pull(repoPath, branch, run){
   let repo, status, err;
   try {
     repo = simpleGit(repoPath);
@@ -13,12 +15,27 @@ async function pull(repoPath, branch){
   } catch(e) {
     err = e;
   }
+  let changed = status.files && status.files.length != 0;
   // only log changes and errors
-  if(status.files && status.files.length != 0){
-    console.log('pull-success', status);
+  if(changed){
+    console.log('pull-success', repoPath, branch, status);
   }
   if(err){
-    console.log('pull-error', err);
+    console.log('pull-error', repoPath, branch, err);
+  }
+  if(changed && run){
+    for(let cmd of run){
+      let err, result = await exec(
+        cmd,
+        {cwd: repoPath}
+      ).catch((e) => err = e);
+      if(result){
+        console.log('Running ', cmd, '\n', result);
+      }
+      if(err){
+         console.log('Error running ', cmd, '\n', err);
+      }
+    }
   }
 }
 
@@ -26,7 +43,14 @@ async function pull(repoPath, branch){
 // and call the pull function
 setInterval(()=>{
   for(let repo of repoInfo){
-    pull(path.join(basePath, repo.path), repo.branch);
+    pull(
+      path.join(basePath, repo.path),
+      repo.branch,
+      repo.run
+    );
   }
 
-}, 600000);
+}, 10000);
+
+
+// cwd = current working directory
